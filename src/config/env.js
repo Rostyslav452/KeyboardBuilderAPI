dff7896735adf14396598eb3bc847312465e86dc
Rostyslav <rooostik2016@gmail.com>
2026-02-20 22:35:58 +0200

    fixed

---
+++ src/config/env.js
@@ -0,0 +1,19 @@
+import logger from "../utils/logger";
+import envSchema from "../validators/schemas/envSchema.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const parsedEnv = envSchema.safeParse(process.env);
+
+if (!parsedEnv.success) {
+    const log = logger.child({ module: "ENV_ERROR_VALIDATION" });
+    log.error(
+        { errors: parsedEnv.error.format() },
+        "Invalid environment variables",
+    );
+
+    process.exit(1);
+}
+
+export const env = parsedEnv.data;
